905572c0b64f91888876d53a3ebad2bc23fc0b3d
quiroz557 <pablopersonal557@gmail.com>
2025-01-23 00:09:08 -0500

    Configuración inicial del proyecto

---
+++ src/main/java/com/example/franchise_project/FranchiseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.franchise_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FranchiseProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/franchise_project/FranchiseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.franchise_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
